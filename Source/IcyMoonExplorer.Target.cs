3a675c9ef2397fc4c6b2ad8a4eb94e1edad21040
tuguzT <timurka.tugushev@gmail.com>
2023-10-23 10:41:26 +0300

    Remove copyright notice for now

--- Source/IcyMoonExplorer.Target.cs
+++ Source/IcyMoonExplorer.Target.cs
@@ -1,7 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class IcyMoonExplorerTarget : TargetRules
 {
