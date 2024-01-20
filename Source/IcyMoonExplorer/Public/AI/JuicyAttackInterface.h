7bdb7528a09eddb7e8b02c9b41012cd76c9f73e6
tuguzT <timurka.tugushev@gmail.com>
2024-01-20 16:52:53 +0300

    Rewrite AI attack interface in C++ for event handling, change colors of enemies

---
+++ Source/IcyMoonExplorer/Public/AI/JuicyAttackInterface.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "JuicyAttackInterface.generated.h"
+
+UDELEGATE(BlueprintCallable)
+DECLARE_DYNAMIC_DELEGATE(FOnAttackEndSignature);
+
+UINTERFACE(BlueprintType, Blueprintable, Category="AI")
+class UJuicyAttackInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class ICYMOONEXPLORER_API IJuicyAttackInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="AI")
+	void Attack(AActor* ActorToAttack);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="AI")
+	void BindEventToOnAttackEnd(const FOnAttackEndSignature& Event);
+};
